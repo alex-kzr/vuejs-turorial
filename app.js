6bd8638346342eada54551e3a28075c76d10fc18
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-21 13:24:04 +1000

    Add dynamic CSS class

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 new Vue({
     el: '#vue-app',
     data: {    
-        a: 0,
-        b: 0,
-        age: 20
+        available: false,
+        nearby: false
     },
     methods: { 
                 
     },
     computed: {
-        addToA: function(){
-            console.log('addToA');
-            return this.age + this.a;
-        },
-        addToB: function(){
-            console.log('addToB');
-            return this.age + this.b;
-        }
+        
     }
 });
