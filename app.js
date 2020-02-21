b77aa3c5308c17aecbd4ad9cfc040e75b4e2458d
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-21 20:07:00 +1000

    Punch bag game. Clear workspace template

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 new Vue({
     el: '#vue-app',
     data: {    
-        name: 'Alex',
-        characters: ['Terminator','Rambo', 'Predator', 'Chuck'],
-        ninjas: [
-            { name: 'Ryu', age: 25 },
-            { name: 'Yoshi', age: 35 },
-            { name: 'Ken', age: 55 }
-        ]
+        
     },
     methods: { 
                 
