fe401baf737fc876cf8eadd2f8de5f723de2106e
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-25 19:54:17 +1000

    Refs. Use ref with div

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ new Vue({
     },
     methods: { 
         readRefs: function(){
-            this.output = this.$refs.input.value;
+            console.log(this.$refs.test.innerText);
         }
     },
     computed: {
