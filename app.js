08a9377ddade07394ea0517eb8964c668e2703dd
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-25 13:01:10 +1000

    Multiple Instances. Add two instances

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-new Vue({
-    el: '#vue-app',
+var one = new Vue({
+    el: '#vue-app-one',
     data: {    
+        title: 'Vue App One'
+    },
+    methods: { 
         
+    },
+    computed: {
+        
+    }
+});
+
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {    
+        title: 'Vue App Two'
     },
     methods: { 
         
