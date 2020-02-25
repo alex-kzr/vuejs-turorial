dbeb654dda06b464f989c425fda92f7a2cd08b4b
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-25 18:53:27 +1000

    Components. Clear

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-var data = {
-    name: 'Yoshi'
-}
-
-Vue.component('greeting',{
-    template: '<p>Hey there, I am {{ name }}. <button v-on:click="changeName">Change name</button></p>',
-    data: function(){
-        return data;
-    },
-    methods: {
-        changeName: function(){
-            this.name = 'Mario'
-        }
-    }
-});
-
-var one = new Vue({
-    el: '#vue-app-one',
+new Vue({
+    el: '#vue-app',
     data: {    
         
     },
@@ -24,18 +8,5 @@ var one = new Vue({
     },
     computed: {
        
-    }
-});
-
-var two = new Vue({
-    el: '#vue-app-two',
-    data: {    
-        
-    },
-    methods: { 
-        
-    },
-    computed: {
-        
     }
 });
