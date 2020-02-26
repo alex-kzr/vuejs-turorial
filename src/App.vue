db7384ff4a5910eabbdfcf777890d60825c5964d
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-26 13:07:48 +1000

    Nesting Components. Add header to the app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  
+  <div>
+    <app-header></app-header>
+  </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+
 export default {
+  components: {
+    'app-header': Header
+  },
   data() {
     return {
       
