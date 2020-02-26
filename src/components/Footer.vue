fdd337a83e4014cdd1baffa67eec7589a3d67c5d
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-26 13:13:49 +1000

    Nesting Components. Add footer to the app

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+      <p>{{ copyright }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      copyright: 'Copyright 2020 Vue Ninjas'
+    }
+  }
+}
+</script scoped>
+
+<style>
+footer {
+    background: #222;
+    padding: 6px;
+}
+p {
+    color: lightgreen;
+    text-align: center;
+}
+h1 {
+    color: #222;
+    text-align: center;
+}
+</style>
