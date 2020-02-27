ba953c7fb7dac9e2dd59c3d38807645905655901
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-27 13:31:23 +1000

    Slots. Place html staff to the nesting component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <form-helper>
-      
+      <div slot="form-header">
+        <h3>This is the title of a form</h3>
+        <p>This is some info about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="text" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
     </form-helper>
   </div>
 </template>
@@ -17,10 +27,18 @@ export default {
     return {
       
     }
+  },
+  methods: {
+    handleSubmit: function(){
+      alert('Thanks for submiting!');
+    }
   }
 }
 </script>
 
 <style>
-
+body{
+  margin: 0;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
+}
 </style>
