58c762ea84112cb067ae3ca1b356466889ec2e2d
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-27 12:49:19 +1000

    Slots. Add formHelper component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  
+  <div>
+    <form-helper></form-helper>
+  </div>
 </template>
 
 <script>
+import formHelper from './components/formHelper.vue'
+
 export default {
+  components: {
+    'form-helper': formHelper
+  },
   data() {
     return {
       
