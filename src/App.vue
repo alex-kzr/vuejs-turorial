30c29775d4b3a4ec0ed8e15a04dd12ba03d39ed5
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-02-27 19:47:06 +1000

    Slots. Clear template

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
   <div>
-    <form-helper>
-      <div slot="form-header">
-        <h3>This is the title of a form</h3>
-        <p>This is some info about the form</p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="name" required />
-        <input type="text" placeholder="password" required />
-      </div>
-      <div slot="form-controls">
-        <button v-on:click="handleSubmit">Submit</button>
-      </div>
-    </form-helper>
+    
   </div>
 </template>
 
 <script>
-import formHelper from './components/formHelper.vue'
-
 export default {
   components: {
-    'form-helper': formHelper
+    
   },
   data() {
     return {
@@ -39,6 +25,5 @@ export default {
 <style>
 body{
   margin: 0;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
 }
 </style>
