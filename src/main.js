100640bf3451c247eb9c1d7477aea83fd76db840
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-04 20:21:34 +1000

    Blog. Add filters

--- src/main.js
+++ src/main.js
@@ -10,18 +10,12 @@ Vue.directive('rainbow',{
   }
 });
 
-Vue.directive('theme',{
-  bind(el, binding, vnode){
-    if (binding.value == 'wide'){
-      el.style.maxWidth = '1200px';
-    }else if(binding.value == 'narrow'){
-      el.style.maxWidth = '560px';
-    }    
-    if (binding.arg == 'column'){
-      el.style.background = '#ddd';
-      el.style.padding = '20px';
-    }
-  }
+Vue.filter('to-uppercase',function(value){
+  return value.toUpperCase();
+});
+
+Vue.filter('snippet',function(value){
+  return value.slice(0,100) + '...';
 });
 
 new Vue({
