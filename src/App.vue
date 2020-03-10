4b65362d5e1c501d2bac5ff0b3287be689bcb211
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-10 19:51:42 +1000

    Blog. Add router links

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import header from './components/header.vue';
 import addBlog from './components/addBlog.vue';
 import showBlogs from './components/showBlogs.vue';
 import listBlogs from './components/listBlogs.vue';
 
 export default {
   components: {
+    'app-header': header,
     'add-blog': addBlog,
     'show-blogs': showBlogs,
     'list-blogs': listBlogs
