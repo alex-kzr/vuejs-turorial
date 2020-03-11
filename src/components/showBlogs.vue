03558416e986995d1360fb403e94e98398cb8f58
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-11 11:06:44 +1000

    Blog. Add retriving data to blog list

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -4,7 +4,7 @@
         <input id="search" type="text" v-model="search" placeholder="search blogs" />
         <div v-for="blog in filteredBlogs" class="single-blog">
             <router-link v-bind:to="'/blog/' + blog.id"><h2 v-rainbow>{{ blog.title | toUppercase }}</h2></router-link>
-            <article>{{ blog.body | snippet }}</article>
+            <article>{{ blog.content | snippet }}</article>
         </div>
     </div>
 </template>
@@ -20,9 +20,15 @@ export default {
         }
     },
     created(){
-        this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
-            console.log(data);
-            this.blogs = data.body.slice(0,10);
+        this.$http.get('https://fb-test-app-1.firebaseio.com/posts.json').then(function(data){
+            return data.json();
+        }).then(function(data){
+            var blogsArray = [];
+            for (let key in data){
+                data[key].id = key;
+                blogsArray.push(data[key]);
+            }
+            this.blogs = blogsArray;
         });
     },
     computed: {
